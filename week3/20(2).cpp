23e60d85171a73559e36908390ad8011ace82762
DELL <DELL@DESKTOP-7K6E1DD>
2021-08-19 15:16:12 +0700

    Add project files.

---
+++ week3/20(2).cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main() {
+
+	int a;
+	scanf("%d", &a);
+	for (int y=1;y<=a;y++) {
+		for (int x=1;x<=a;x++) {
+				/*printf("(%d,%d) ",x,y);*/
+				if (x <= a && y == 1) {
+					printf("*");
+				} 
+				else {
+					printf(" ");
+				}
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
---
+++ week3/20.cpp
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main() {
+	
+	int a;
+	scanf("%d", &a);
+	for (int i=0;i<a;i++) {
+
+		printf("*");
+
+	}
+
+	return 0;
+}
